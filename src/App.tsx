429bebb3cd5b8492bd04172681ffc4e1d6531dba
Koya <44787464+koyaboy@users.noreply.github.com>
2024-05-28 10:17:58 +0100

    Implement header component design for desktop view

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
-import "./App.scss";
+import Header from "./components/Header/Header";
 
 function App() {
   return (
     <>
-      <h1 className="hello">Hello World</h1>
+      <Header />
     </>
   );
 }
---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,66 @@
+import React from "react";
+import "./Header.scss";
+import logo from "../../assets/logo.png";
+import userImage from "../../assets/user-image.png";
+import notificationBell from "../../assets/notification-bell.png";
+
+const Header = () => {
+  return (
+    <header className="header">
+      <img src={logo} alt="Lendsqr Logo" className="logo" />
+
+      <div className="search-container">
+        <input type="text" placeholder="Search for anything" />
+        <div className="search-button-container">
+          <svg
+            width="14"
+            height="14"
+            viewBox="0 0 14 14"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M5.3541 0.000553316C3.94043 0.0214743 2.59056 0.59363 1.5911 1.59554C0.572324 2.6165 0 4.00108 0 5.44478C0 6.88848 0.572324 8.27307 1.5911 9.29402C2.5152 10.2183 3.74056 10.7782 5.04297 10.8714C6.34537 10.9645 7.6377 10.5847 8.68348 9.80138L12.874 14L13.9717 12.9002L9.77963 8.70008C10.5612 7.65258 10.9403 6.35818 10.8476 5.05362C10.7549 3.74905 10.1966 2.52153 9.27477 1.59554C8.76094 1.08047 8.1492 0.673917 7.47576 0.39995C6.80232 0.125984 6.08086 -0.00982865 5.3541 0.000553316ZM5.48903 1.55605C6.49887 1.57093 7.46314 1.97962 8.1771 2.69533C8.9048 3.42458 9.3136 4.41357 9.3136 5.44478C9.3136 6.476 8.9048 7.46498 8.1771 8.19424C7.44925 8.92334 6.46216 9.33293 5.43293 9.33293C4.4037 9.33293 3.41662 8.92334 2.68877 8.19424C1.96107 7.46498 1.55227 6.476 1.55227 5.44478C1.55227 4.41357 1.96107 3.42458 2.68877 2.69533C3.05576 2.32744 3.49268 2.03706 3.97367 1.84137C4.45466 1.64568 4.96995 1.54866 5.48903 1.55605Z"
+              fill="white"
+            />
+          </svg>
+        </div>
+      </div>
+
+      <div className="header-actions">
+        <a href="/" className="docs-link">
+          Docs
+        </a>
+        <div className="user-controls">
+          <img src={notificationBell} alt="Notification Bell Icon" />
+          <div className="user-profile">
+            <div>
+              <img src={userImage} alt="User Image" />
+            </div>
+            <button>
+              <span className="user-name">Adedeji</span>
+              <span className="dropdown-container">
+                <svg
+                  width="8"
+                  height="5"
+                  viewBox="0 0 8 5"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    fill-rule="evenodd"
+                    clip-rule="evenodd"
+                    d="M3.39229 4.0516C3.72823 4.42504 4.27511 4.42192 4.60791 4.0516L7.48291 0.856996C7.81885 0.484336 7.68525 0.181995 7.18447 0.181995H0.815667C0.314887 0.181995 0.183627 0.487456 0.517227 0.856996L3.39229 4.0516Z"
+                    fill="#213F7D"
+                  />
+                </svg>
+              </span>
+            </button>
+          </div>
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;

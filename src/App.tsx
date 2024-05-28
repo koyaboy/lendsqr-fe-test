162ef0fb1f3dc95e6d87705f5ea81a2719daca3a
Koya <44787464+koyaboy@users.noreply.github.com>
2024-05-28 12:27:49 +0100

    Implement sidebar component design

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,17 @@
 import Header from "./components/Header/Header";
+import Sidebar from "./components/Sidebar/Sidebar";
+import "./App.scss";
 
 function App() {
   return (
     <>
       <Header />
+      <div className="content">
+        <Sidebar />
+        <main>
+          <div>TEST</div>
+        </main>
+      </div>
     </>
   );
 }

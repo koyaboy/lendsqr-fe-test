41d5643cb76d0f710e4dc389dd339d6f722543f4
Koya <44787464+koyaboy@users.noreply.github.com>
2024-05-29 14:25:49 +0100

    Implement responsive table design in Users page

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import Header from "./components/Header/Header";
 import Sidebar from "./components/Sidebar/Sidebar";
+import Users from "./pages/Users/Users";
 import "./App.scss";
 
 function App() {
@@ -9,7 +10,7 @@ function App() {
       <div className="content">
         <Sidebar />
         <main>
-          <div>TEST</div>
+          <Users />
         </main>
       </div>
     </>

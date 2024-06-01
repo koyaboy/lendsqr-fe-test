5f2c06889831e5605c7621f5b49782c47ccb4710
Koya <44787464+koyaboy@users.noreply.github.com>
2024-06-01 19:45:30 +0100

    Add Form Validation to Login Form

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -3,7 +3,27 @@ import "./Login.scss";
 import logo from "../../assets/logo.png";
 import SignInImage from "../../assets/sign-in-img.png";
 
+import { useForm, SubmitHandler } from "react-hook-form";
+
+type LoginFormInput = {
+  email: string;
+  password: string;
+};
+
+const loginCredentials = {
+  email: "admin@test.com",
+  password: "admin",
+};
+
 const Login = () => {
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm<LoginFormInput>();
+  const onSubmit: SubmitHandler<LoginFormInput> = (data) => console.log(data);
+
   return (
     <main className="login">
       <img src={logo} alt="Logo" className="logo" />
@@ -12,7 +32,7 @@ const Login = () => {
         <img src={SignInImage} alt="Sign In Image" className="sign-in-img" />
       </div>
 
-      <form>
+      <form onSubmit={handleSubmit(onSubmit)}>
         <div className="login-container">
           <div className="heading-section">
             <h1>Welcome!</h1>
@@ -20,17 +40,54 @@ const Login = () => {
           </div>
 
           <div className="inputs">
-            <input type="text" placeholder="Email" />
+            <input
+              type="email"
+              placeholder="Email"
+              {...register("email", {
+                required: true,
+                pattern: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/,
+              })}
+              aria-invalid={errors.email ? "true" : "false"}
+            />
+            {errors.email?.type === "required" && (
+              <p role="alert" className="validation-error-message">
+                Email is required
+              </p>
+            )}
+            {errors.email?.type === "pattern" && (
+              <p role="alert" className="validation-error-message">
+                Enter a valid email address
+              </p>
+            )}
             <div className="password-container">
-              <input type="text" placeholder="Password" />
+              <input
+                type="password"
+                placeholder="Password"
+                {...register("password", { required: true })}
+                aria-invalid={errors.password ? "true" : "false"}
+              />
               <button className="show">SHOW</button>
             </div>
+            {errors.password?.type === "required" && (
+              <p role="alert" className="validation-error-message">
+                Password is required
+              </p>
+            )}
             <a href="/" className="forgot-password">
               FORGOT PASSWORD?
             </a>
           </div>
 
-          <button className="login-btn">LOG IN</button>
+          <button
+            className="login-btn"
+            style={{
+              opacity: errors.email || errors.password ? "0.5" : "1",
+              cursor:
+                errors.email || errors.password ? "not-allowed" : "pointer",
+            }}
+          >
+            LOG IN
+          </button>
         </div>
       </form>
     </main>

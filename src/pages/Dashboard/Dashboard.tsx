6d2a7eedce85e85d9457a99111180dfd74e18d91
Koya <44787464+koyaboy@users.noreply.github.com>
2024-06-01 07:51:43 +0100

    Hide sidebar on tablet and mobile screens

--- src/pages/Dashboard/Dashboard.tsx
+++ src/pages/Dashboard/Dashboard.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Header from "../../components/Header/Header";
 import Sidebar from "../../components/Sidebar/Sidebar";
+import "./Dashboard.scss";
 import { Outlet } from "react-router-dom";
 
 const Dashboard = () => {
@@ -8,7 +9,9 @@ const Dashboard = () => {
     <>
       <Header />
       <div className="content">
-        <Sidebar />
+        <div className="sidebar">
+          <Sidebar />
+        </div>
         <main>
           <Outlet />
         </main>
